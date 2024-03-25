4b3c8e0e7682199b834809d869a796f3e2f6ce44
zhousui <zhousui@ZS>
2024-03-25 18:33:37 +0800

    添加数据链接部分模块

---
+++ CommnoInitializer/WebApplicationBuilderExtensions.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.Configuration;
+using AnyDBConfigProvider;
+using System.Data.SqlClient;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace CommnoInitializer
+{
+    public class WebApplicationBuilderExtensions
+    {
+        public static void ConfigureDbConfiguration(this WebApplicationBuilder builder)
+        {
+            //不能使用ConfigureAppConfiguration中的configBuilder去读取配置，否则就循环调用了，因此这里直接自己去读取配置文件
+            //var configRoot = new ConfigurationBuilder().AddJsonFile("appsettings.json").Build();
+            //string connStr = configRoot.GetValue<string>("DefaultDB:ConnStr");
+
+            builder.Host.ConfigureAppConfiguration((hostCtx, configBuild) =>
+            {
+                string connStr = builder.Configuration.GetValue<string>("DefaultDB:ConnStr");
+                configBuild.AddDbConfiguration(() => new SqlConnection(connStr), reloadOnChange: true, reloadInterval: TimeSpan.FromSeconds(5));
+            });
+        }
+
+        public static void ConfigExtraServices(this WebApplicationBuilder builder, InitializerOptions initializerOptions)
+        {
+            IServiceCollection services = builder.Services;
+            IConfiguration configuration = builder.Configuration;
+
+        }
+
+    }
+}
